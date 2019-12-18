9dd44d345b5d4e7fce38d944e1eb7249f551eda0
Joseph Ngugi <jngugi88@gmail.com>
2019-12-18 14:21:48 +0300

    add svg

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,14 @@
+interface ISvg {
+  splash: string;
+  logo: string;
+  search: string
+}
+
+const svg = {
+  splash: require('./splash.svg'),
+  logo: require('./logo.svg'),
+  search: require('./search.svg'),
+  
+};
+
+export default svg;
---
+++ src/assets/theme/colors.ts
@@ -0,0 +1,6 @@
+export default {
+    darkBlue: '#283346',
+    green: '#4FCE5D',
+    chatBubbleGrey: '#F5F2F2',
+    chatBubbleDark: '#354258',
+}
