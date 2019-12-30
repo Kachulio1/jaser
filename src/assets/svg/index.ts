f962072c718147e86348841adc83092371c4e728
Joseph Ngugi <jngugi88@gmail.com>
2019-12-30 17:03:32 +0300

    add edit svg

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,14 +1,17 @@
 interface ISvg {
   splash: string;
   logo: string;
-  search: string
+  search: string;
+  addSmall: string;
+  edit: string;
 }
 
 const svg = {
   splash: require('./splash.svg'),
   logo: require('./logo.svg'),
   search: require('./search.svg'),
-  
+  addSmall: require('./add-small.svg'),
+  edit: require('./edit.svg'),
 };
 
 export default svg;
