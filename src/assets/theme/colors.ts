e6232ab447106fcf4c4981bdf11e42531a27acd4
Joseph Ngugi <jngugi88@gmail.com>
2019-12-30 17:03:50 +0300

    add color variables

--- src/assets/theme/colors.ts
+++ src/assets/theme/colors.ts
@@ -1,6 +1,10 @@
 export default {
     darkBlue: '#283346',
     green: '#4FCE5D',
+    darkGreen: '#7BBE3C',
     chatBubbleGrey: '#F5F2F2',
     chatBubbleDark: '#354258',
+    greyWhite: '#E4E4E4',
+    white: '#fff',
+    statusColor: '#707070'
 }
